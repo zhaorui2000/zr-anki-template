f123d1e78618e7023c13168a5fe911b5548c669c
Zhao Rui <wiserui1996@hotmail.com>
2024-11-10 14:59:24 +0800

    fix: 修复填空题 背面 换行符显示

--- src/填空题/reverse.jsx
+++ src/填空题/reverse.jsx
@@ -6,13 +6,33 @@ export default function App() {
   const cardOrder = `{{#c1}}1{{/c1}}{{#c2}}2{{/c2}}{{#c3}}3{{/c3}}{{#c4}}4{{/c4}}{{#c5}}5{{/c5}}{{#c6}}6{{/c6}}{{#c7}}7{{/c7}}{{#c8}}8{{/c8}}{{#c9}}9{{/c9}}{{#c10}}10{{/c10}}`;
   const questionArray = "{{问题}}".split(/\{\{|\}\}/);
   const extension = "{{补充}}";
+
+  const renderText = function (value) {
+    if (value.indexOf("<br>") !== -1) {
+      let arr = value.split("<br>").filter((item) => !!item);
+      return arr.map((item, index) => {
+        if (index === 0 && arr.length > 1) {
+          return <span>{item}</span>;
+        } else {
+          return (
+            <>
+              <br></br>
+              <span>{item}</span>
+            </>
+          );
+        }
+      });
+    }
+    return <span>{value}</span>;
+  };
+
   return (
     <div className="grid gap-y-2 px-2">
       <Card title="填空" actions={<Tags></Tags>}>
         <div>
           {questionArray.map((item, index) => {
             if (index % 2 === 0) {
-              return <span>{item}</span>;
+              return renderText(item);
             }
             if (Number(item.slice(1, 2)) === Number(cardOrder)) {
               return (
@@ -21,7 +41,7 @@ export default function App() {
                 </MaskButton>
               );
             } else {
-              return <span>{item.slice(4)}</span>;
+              return renderText(item.slice(4));
             }
           })}
         </div>
