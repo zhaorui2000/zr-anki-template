7fa9ea31631bc16eca43e7431f6c72b28f6047d7
Zhao Rui <wiserui1996@hotmail.com>
2024-11-19 15:06:37 +0800

    fix: 修复卡片 base 样式

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Content from "./填空题/reverse";
 
 export default function App() {
   return (
-    <div className="h-svh w-svw bg-base-100">
+    <div className="h-svh w-svw bg-base-100 text-base-content">
       <RandomThemeButton></RandomThemeButton>
       <Content></Content>
     </div>
