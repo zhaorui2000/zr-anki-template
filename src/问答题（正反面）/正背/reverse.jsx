45f632ec11f2a0ae5876d47b278fca2cde2164d1
Zhao Rui <wiserui1996@hotmail.com>
2025-01-12 22:17:54 +0800

    feat: 问答题，全部背面改进

--- src/问答题（正反面）/正背/reverse.jsx
+++ src/问答题（正反面）/正背/reverse.jsx
@@ -2,6 +2,7 @@ import Card from "../../components/Card";
 import useAnkiText from "../../hooks/useAnkiText";
 import Tags from "../../modules/Tags";
 import PromptCard from "../../modules/PromptCard";
+import BottomAlertWrap from "../../modules/BottomAlertWrap";
 
 export default function App() {
   const question = useAnkiText("{{正面问题}}");
@@ -9,17 +10,19 @@ export default function App() {
   const prompt = useAnkiText("{{提示}}");
   const extension = useAnkiText("{{补充}}");
   return (
-    <div className="gap-y-2 flex flex-col p-4">
-      <Card title="问题" color="primary">
-        {question}
-      </Card>
-      <PromptCard></PromptCard>
-      <Card title="答案" actions={<Tags></Tags>}>
-        {answer}
-      </Card>
-      <Card title="补充" color="warning">
-        {extension}
-      </Card>
-    </div>
+    <BottomAlertWrap>
+      <div className="gap-y-2 flex flex-col p-4">
+        <Card title="问题" color="primary">
+          {question}
+        </Card>
+        <PromptCard></PromptCard>
+        <Card title="答案" actions={<Tags></Tags>}>
+          {answer}
+        </Card>
+        <Card title="补充" color="warning">
+          {extension}
+        </Card>
+      </div>
+    </BottomAlertWrap>
   );
 }
